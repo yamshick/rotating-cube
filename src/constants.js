3812eecbe26f2cf3cb7b19b1acfff860ab92afc7
danila <danila.it@rambler.ru>
2023-01-16 23:35:23 +0300

    refactoring

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
-var W = 600, H = 600;
-var MODEL_MIN_X = -2, MODEL_MAX_X = 2;
-var MODEL_MIN_Y = -2, MODEL_MAX_Y = 2;
+const W = 600, H = 600;
+const MODEL_MIN_X = -2, MODEL_MAX_X = 2;
+const MODEL_MIN_Y = -2, MODEL_MAX_Y = 2;
 
-var CANVAS = document.getElementById('canvas');
-var CONTEXT = canvas.getContext('2d');
+const CANVAS = document.getElementById('canvas');
+const CONTEXT = canvas.getContext('2d');
