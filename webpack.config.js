416ef091ed427b29126eea7e2ddddf7b9ad10939
danila <danila.it@rambler.ru>
2023-01-17 00:37:42 +0300

    style loading

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     entry: "/src/index.js",
@@ -17,21 +18,23 @@ module.exports = {
                 exclude: /node_modules/,
                 use: {
                     loader: "babel-loader",
-                    // "options": {
-                    //     "presets": [ "@babel/preset-env", ]
-                    // }
                 },
             },
-            // {
-            //     test: /\.css$/,
-            //     use: [
-            //         "style-loader",
-            //         "css-loader", // for styles
-            //     ],
-            // },
+            {
+                test: /\.css$/,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                ],
+            },
         ],
     },
     resolve: {
         extensions: [".js"],
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+        }),
+    ]
 };
